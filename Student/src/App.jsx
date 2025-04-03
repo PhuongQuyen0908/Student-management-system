5fb36f6840071446a4e89ddbf035a59e2361595f
BPhucKHMT <23521208@gm.uit.edu.vn>
2025-04-03 21:53:13 +0700

    update

--- Student/src/App.jsx
+++ Student/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <div className="app-container">
         {/* App Route */}
         <ModalAddStudent />
-        <ModalAddStudent />
+        <ModalUpdateStudent />
         <AppRoutes />
       </div>
       <ToastContainer
